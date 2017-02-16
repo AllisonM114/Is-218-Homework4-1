f7a15478f3a06c8d0be6abab356fa447975988eb
Allison MAnzo <adm52@njit.edu>
2017-02-15 20:26:30 -0500

    add into repo

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+    $dsn = 'mysql:host=sql1.njit.edu;dbname=adm52';
+    $username = 'adm52';
+    $password = '55G0oL4D';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('database_error.php');
+        exit();
+    }
+?>
